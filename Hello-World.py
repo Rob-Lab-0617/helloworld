4f0525de745904ba674df5ef40464c6ebb0628a8
Rob-Lab-0617 <shujismp@gmail.com>
2025-05-25 06:54:11 +0900

    Update Hello-World.py

--- Hello-World.py
+++ Hello-World.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 st.title('Hello World')
-st.header('Streamlitを使って作りました')
+st.header('Streamlitを使って作りました。')
+st.subheader(
+  body='このサイトはShujiによって  作られました''<h3>',
+  anchor='title'
+)
 st.snow()
